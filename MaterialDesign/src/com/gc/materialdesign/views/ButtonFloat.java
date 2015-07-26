23a4e4c999264fdd44c31d01a5a3579bc439319d
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-07-26 12:08:30 -0400

    Fixed the naming collision with new design support library, fixed the landscape color chooser

--- MaterialDesign/src/com/gc/materialdesign/views/ButtonFloat.java
+++ MaterialDesign/src/com/gc/materialdesign/views/ButtonFloat.java
@@ -88,12 +88,12 @@ public class ButtonFloat extends Button{
 		// Set Ripple Color
 		// Color by resource
 		int rippleColor = attrs.getAttributeResourceValue(MATERIALDESIGNXML,
-				"rippleColor", -1);
+				"md_rippleColor", -1);
 		if (rippleColor != -1) {
 			setRippleColor(getResources().getColor(rippleColor));
 		} else {
 			// Color by hexadecimal
-			int background = attrs.getAttributeIntValue(MATERIALDESIGNXML, "rippleColor", -1);
+			int background = attrs.getAttributeIntValue(MATERIALDESIGNXML, "md_rippleColor", -1);
 			if (background != -1)
 				setRippleColor(background);
 			else
